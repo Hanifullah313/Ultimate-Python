9081e5e842ab2e0e708f3509960d1bd877239942
hanifullah313 <hu4206835@gmail.com>
2025-10-31 02:10:52 -0700

    Chapter 8 Added

---
+++ Chapter 8/fun.py
@@ -0,0 +1,10 @@
+def avg():
+    a = int(input("enter number "))
+    b = int(input("enter number "))
+    c = int(input("enter number "))
+    average = (a + b + c) / 3
+    print("Average is:", average)
+
+
+
+avg()
