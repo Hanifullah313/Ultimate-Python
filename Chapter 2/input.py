4a959e7fb1dfdebb6f97f34e62de83e189bf7d8e
hanifullah313 <hu4206835@gmail.com>
2025-10-30 21:13:59 -0700

    Chapter 2 Added

---
+++ Chapter 2/input.py
@@ -0,0 +1,6 @@
+a = int(input("Enter an Integer :"))
+b = int(input("Enter an Integer :"))
+
+print("A is greater than B 2", a>b) 
+
+print ("the meean of a and b is :", (a+b)/2 )
---
+++ Chapter 2/var_dataTypes.py
@@ -0,0 +1,17 @@
+# varaibles and data types
+a = 10  # integer
+b = 5.5  # float    
+c = "Hello, World!"  # string
+d = True  # boolean
+e = [1, 2, 3, 4, 5]  # list
+f = (1, 2, 3)  # tuple
+g = { "name": "Alice", "age": 25 }  # dictionary
+h = {1, 2, 3, 4, 5}  # set
+print(type(a))
+print(type(b))  
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
